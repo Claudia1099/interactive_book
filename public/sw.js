014eec1ee7df1cebfa541aaad71c57652b68d816
mpadilla <mipadilla.o@gmail.com>
2021-02-26 01:08:43 -0500

    actualizacion sw, index, manifest.

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,24 @@
 UpUp.start({
+    'cache-version':'v1',
     'content-url':'index.hml',
     'assets':[
-        'index.html',
+        /* logo-ico */
         'aesLogo.png',
         'aesLogoHeader.png',
-        'base.css',
-        'book.css',
-        'favicon.ico',
-        'libro.css',
         'logo192.png',
         'logo512.png',
+        'favicon.ico',
+        
+        /* css */
+        'base.css',
+        'book.css',
+        'libro.css',
         'main.css',
+        'css/all.css',
+        'static/css/main.b596d975.chunk.css',
+        'static/css/bootstrap.css',
+        
+        /* js */
         'upup.sw.min.js',
         'upup.min.js',
         'sw.js',
@@ -18,24 +26,72 @@ UpUp.start({
         'static/js/3.3fcf9b6a.chunk.js',
         'static/js/main.87ceb43f.chunk.js',
         'static/js/runtime-main.990a9f21.js',
-        'static/css/main.f9c0ebff.chunk.css',
+
+        /* map */
+        'upup.min.js.map',
+        'upup.sw.min.map',
+        'static/css/main.b596d975.chunk.css.map',
+        'static/js/2.2128f5f6.chunk.map',
+        'static/js/3.3fcf9b6a.chunk.map',
+        'static/js/main.87ceb43f.chunk.map',
+        'static/js/runtime-main.990a9f21.map',
+
+        /* json */
+        'manifest.json',
+        
+        /* font-aweson-ttf */
         'static/media/Courgette-Regular.af9c15cb.ttf',
-        'css/all.css',
-        'webfonts/fa-brands-400.eot',
-        'webfonts/fa-brands-400.svg',
         'webfonts/fa-brands-400.ttf',
-        'webfonts/fa-brands-400.woff',
-        'webfonts/fa-brands-400.woff2',
-        'webfonts/fa-regular-400.eot',
-        'webfonts/fa-regular-400.svg',
         'webfonts/fa-regular-400.ttf',
-        'webfonts/fa-regular-400.woff',
-        'webfonts/fa-regular-400.woff2',
-        'webfonts/fa-solid-900.eot',
-        'webfonts/fa-solid-900.svg',
         'webfonts/fa-solid-900.ttf',
+        /* font-aweson-eot */
+        'webfonts/fa-brands-400.eot',
+        'webfonts/fa-regular-400.eot',
+        'webfonts/fa-solid-900.eot',
+        /* font-aweson-svg */
+        'webfonts/fa-brands-400.svg',
+        'webfonts/fa-regular-400.svg',
+        'webfonts/fa-solid-900.svg',
+        /* font-aweson-woff */
+        'webfonts/fa-brands-400.woff',
+        'webfonts/fa-regular-400.woff',
         'webfonts/fa-solid-900.woff',
+        /* font-aweson-woff2 */
+        'webfonts/fa-brands-400.woff2',
+        'webfonts/fa-regular-400.woff2',
         'webfonts/fa-solid-900.woff2',
-        'media/vide2.mp4' 
+        
+        /* media */
+        'media/video2.mp4',
+
+        /* img */
+        'img/book1.png',
+        'img/book2.png',
+        'img/book3.png',
+        'img/book4.png',
+        'img/book5.png',
+        'img/courseManicure.png',
+        'img/courseRecepcion.png',
+        'img/courseShef.png',
+        'img/courses/portadaCursoBasico.png',
+        'img/courses/time1.png',
+        'img/courses/time2.png',
+        'img/courses/time3.png',
+        'img/courses/time4.png',
+        'img/courses/time5.png',
+        'img/courses/time6.png',
+        'img/courses/time7.png',
+        'img/courses/time8.png',
+        'img/courses/time9.png',
+        
+        /* audio */
+        'audio/time1.mp3',
+        'audio/time2.mp3',
+        'audio/time3.mp3',
+        'audio/time4.mp3',
+        'audio/time5.mp3',
+        'audio/time6.mp3',
+        'audio/time7.mp3',
+        'audio/time8.mp3'
     ]
 });
