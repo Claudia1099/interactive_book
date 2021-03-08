54749449f1dc825d5897286e47b3b75bccf11085
Nelson Zambrano <nelsonzambrano23@gmail.com>
2021-03-07 23:22:33 -0500

    Actualizar libro 1 audios, inicio libro 2

--- src/component/EBooks.js
+++ src/component/EBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Lista from "./Lista";
 import Ebook1 from "./Ebook1";
+import EBook2 from "./Ebook2";
 
 class EBooks  extends Component{
 
@@ -9,13 +10,15 @@ class EBooks  extends Component{
         this.state={
           listaEBooksVw:true,
           eBooks1Vw:false,
+          eBooks2Vw:false
         }
     }
     
-    abrirEBooks(){
+    abrirEBooks(listaBook, book1, book2){
         this.setState({
-            listaEBooksVw:false,
-            eBooks1Vw:true,
+            listaEBooksVw: listaBook,
+            eBooks1Vw: book1,
+            eBooks2Vw: book2
         })
       }
 
@@ -29,8 +32,8 @@ class EBooks  extends Component{
                         <div className="Cursos">
                             <h3>E-BOOKS</h3>
                             <div className="Lista-cursos">
-                                <span onClick={() => this.abrirEBooks()}><Lista courses="Book 1" imgurl="./img/book1.png" imgalt="Book 1"/></span>
-                                <span ><Lista courses="Book 2" imgurl="./img/book2.png" imgalt="Book 2"/></span>
+                                <span onClick={() => this.abrirEBooks(false, true, false)}><Lista courses="Book 1" imgurl="./img/book1.png" imgalt="Book 1"/></span>
+                                <span onClick={() => this.abrirEBooks(false, false, true)}><Lista courses="Book 2" imgurl="./img/book2.png" imgalt="Book 2"/></span>
                                 <span ><Lista courses="Book 3" imgurl="./img/book3.png" imgalt="Book 3"/></span>
                                 <span ><Lista courses="Book 4" imgurl="./img/book4.png" imgalt="Book 4"/></span>
                             </div>
@@ -41,6 +44,11 @@ class EBooks  extends Component{
                     {
                         this.state.eBooks1Vw?
                         <Ebook1/>
+                        :null
+                    }
+                    {
+                        this.state.eBooks2Vw?
+                        <EBook2/>
                         :null
                     }
                     
