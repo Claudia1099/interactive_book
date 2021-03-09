c8ee867b8dfd1052b42b11d5de5b87b59a160746
mpadilla <mipadilla.o@gmail.com>
2021-03-09 17:06:23 -0500

    Prueba Iframe.... Libro 2

--- src/component/EBooks.js
+++ src/component/EBooks.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Lista from "./Lista";
 import Ebook1 from "./Ebook1";
+import Ebook2 from "./Ebook2";
+
 
 class EBooks  extends Component{
 
@@ -9,13 +11,21 @@ class EBooks  extends Component{
         this.state={
           listaEBooksVw:true,
           eBooks1Vw:false,
+          eBooks2Vw:false,
         }
     }
     
-    abrirEBooks(){
+    eBooks1Vw(){
         this.setState({
             listaEBooksVw:false,
-            eBooks1Vw:true,
+            eBooks1Vw:true
+        })
+      }
+      eBooks2Vw(){
+        this.setState({
+            listaEBooksVw:false,
+            eBooks1Vw:false,
+            eBooks2Vw:true
         })
       }
 
@@ -29,8 +39,8 @@ class EBooks  extends Component{
                         <div className="Cursos">
                             <h3>E-BOOKS</h3>
                             <div className="Lista-cursos">
-                                <span onClick={() => this.abrirEBooks()}><Lista courses="Book 1" imgurl="./img/book1.png" imgalt="Book 1"/></span>
-                                <span ><Lista courses="Book 2" imgurl="./img/book2.png" imgalt="Book 2"/></span>
+                                <span onClick={(e) => this.eBooks1Vw()}><Lista courses="Book 1" imgurl="./img/book1.png" imgalt="Book 1"/></span>
+                                <span onClick={(e) => this.eBooks2Vw()}><Lista courses="Book 2" imgurl="./img/book2.png" imgalt="Book 2"/></span>
                                 <span ><Lista courses="Book 3" imgurl="./img/book3.png" imgalt="Book 3"/></span>
                                 <span ><Lista courses="Book 4" imgurl="./img/book4.png" imgalt="Book 4"/></span>
                             </div>
@@ -43,7 +53,12 @@ class EBooks  extends Component{
                         <Ebook1/>
                         :null
                     }
-                    
+                    {
+                        this.state.eBooks2Vw?
+                        <Ebook2/>
+                        :null
+                    }
+
                 </div>
             </div>           
         );
--- src/component/Ebook2.js
+++ src/component/Ebook2.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 
-class EBooks  extends Component{
+class EBooks2  extends Component{
     render(){
         return(
-            <div className="App-curso App-cont">
-                <div className="App-mg">
-
-                    <div className="Cursos">
-                        <h3>En Construccion</h3>
-                    </div>
+            <div className="libro">
+                {/* <div className="Portada">
+                    <img src='./img/courses/portadaCursoBasico.png'className="App-portada" alt="Portada"/>
+                </div> */}
+                <div className="Hoja">
+                    <iframe id="book2" className="iframeBook" title="ebook 2" sandbox src='./book-one-page/book2/book2.html'></iframe>
                 </div>
-            </div>           
+            </div>
         );
     }
 }
 
-export default EBooks;
+export default EBooks2;
