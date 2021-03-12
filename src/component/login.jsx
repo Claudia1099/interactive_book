7b95c88030e345b96d43cd54517a5bcc498f074a
Miguel Padilla <mipadilla@gmail.com>
2021-03-12 10:35:12 -0500

    trabajos en el login

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 
-class login extends Component{
+class Login extends Component{
+    
     render(){
         return(
-            <div className="App-lista">
-                <div className="cont-lista">
-                    <img src={this.props.imgurl} alt={this.props.imgalt}></img>  
+            <div className="App-cont">
+                <div className="">
+                    <form onSubmit={this.props.onSubmit}>
+                        <div className="card">
+                            <img src="./img/backLogin.png" className="card-img-top" alt="backLogin"></img>
+                            <div className="card-body">
+                                <input className="inpt-login form-control" type="text" id="username" label="user name" placeholder="Username"/>
+                                <input className="inpt-login form-control" type="password" id="password" label="password" placeholder="Password"/>
+                            </div>
+                        </div>
+                        <button className="btn-login btn btn-primary" type="submit">Sign In</button>
+                    </form>
                 </div>
-                <span><p>{this.props.courses}</p></span>
             </div>
         );
     }
 }
 
-export default login;
+export default Login;
