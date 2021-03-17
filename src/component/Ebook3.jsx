40cc042044906c5d371b856680f59a59916c903c
Nelson <nelsonzambrano23@gmail.com>
2021-03-17 10:21:17 -0500

    Actualizacion libro 1 y 3

--- src/component/Ebook3.jsx
+++ src/component/Ebook3.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Swal  from "sweetalert2";
+import "./Ebook3/contents.css";
 
 class EBook3 extends Component{
     
     render(){
     return(
         <div>
-            <h1>Hola mundo</h1>
+                <iframe width="1000" height="550" allowfullscreen="" src="../../book-one-page/book3/book3.html" frameborder="0"></iframe>
         </div>
     );
     }
