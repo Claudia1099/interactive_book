84c9dd6fa391796e89e3e2f65a35723a0f2df02c
Miguel Padilla <mipadilla@gmail.com>
2021-03-17 17:48:20 -0500

    insercion iframe

--- src/component/Ebook3.jsx
+++ src/component/Ebook3.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import "./iframe/iframe.css";
 
 class EBook3 extends Component{
     
     render(){
     return(
-        <div>
-            <h1>En Proceso...</h1>
-        </div>
-    );
+        <div className="">
+            <iframe className="ifm" title="book3" 
+                src="./book-one-page/book3/book3.html"
+                allow="fullscreen" scrolling="no" 
+                sandbox="allow-forms allow-modals allow-popups allow-same-origin" >
+            </iframe>
+        </div>            
+        );
     }
 }
 
