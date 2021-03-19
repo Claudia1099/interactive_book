b7884c34fdd0095566e5f2e0ce121deef602ed7f
Miguel Padilla <mipadilla@gmail.com>
2021-03-19 10:41:50 -0500

    insercion libro cuatro a jsx.

---
+++ src/component/Ebook4.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Book4 from "./Ebook4/Book4"
+
+class Ebook4 extends Component{
+    
+    render(){
+        return(
+            <div className="App-cont">
+                <Book4/>
+            </div>
+        );
+    }
+}
+
+export default Ebook4;
