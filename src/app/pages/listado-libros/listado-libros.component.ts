4b0fe3c92357a6f8a85bfffd1a4e86b71bf45b4a
claudia <claudia.benavidesvargas@gmail.com>
2022-10-01 16:44:43 -0500

    commit 1

--- src/app/pages/listado-libros/listado-libros.component.ts
+++ src/app/pages/listado-libros/listado-libros.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Libros } from 'src/app/models/libros';
 import { AuthService } from '../../services/auth.service';
+import LibrosJSON from '../../../assets/json/libros.json';
+import { ListadoLibros } from '../../models/libros-json';
 
 @Component({
   selector: 'app-listado-libros',
@@ -9,14 +11,41 @@ import { AuthService } from '../../services/auth.service';
 })
 export class ListadoLibrosComponent implements OnInit {
   constructor(private auth: AuthService) {}
-  libros: Libros | undefined;
+  libros!: Libros;
+
+  auxLibros: any[] = [];
+  listadoLibros: ListadoLibros[] = LibrosJSON;
 
   ngOnInit(): void {
     this.auth.user.subscribe(
       (resp: any) => {
         this.auth.librosUsuario(resp.email).subscribe((libros: any) => {
           this.libros = libros.payload.data();
-          console.log(this.libros);
+          Object.values(this.libros).forEach((resp, index) => {
+            if (typeof resp === 'boolean') {
+              Object.keys(this.libros).forEach((property, indice) => {
+                if (index === indice) {
+                  this.auxLibros.push({
+                    [property]: resp,
+                    property: property,
+                  });
+                }
+              });
+            }
+          });
+          this.auxLibros.sort((a, b) => {
+            if (a.property > b.property) {
+              return 1;
+            }
+            if (a.property < b.property) {
+              return -1;
+            }
+            return 0;
+          });
+          this.listadoLibros.forEach((listado, index) => {
+            listado.libro =
+              this.auxLibros[index][this.auxLibros[index].property];
+          });
         });
       },
       (errors: any) => {
